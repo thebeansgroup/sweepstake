9033ebe8740793e70f5d9393dfe534e3696c60cf
Vincent Siebert <vincent@siebert.im>
2012-06-06 13:41:23 +0100

    adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sweepstake::Application.config.secret_token = '325c09738fb6627ba578a307cf3640d8041f546e9addfa639a175dd734a5824894fc680b71149eee9fe1b50b5aa8b67a7eb2a34b9fe23dbe4a8afb7a22255c88'
